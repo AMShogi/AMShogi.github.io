e16d17a303779781afb946e03701aa2a9cbdb28f
Little-Mage <lm4ssw@gmail.com>
2023-03-11 16:31:15 -0700

    idk

---
+++ handicap-calc/hc-calc.js
@@ -0,0 +1,12 @@
+function handicap_input(message) {
+    let rank_input = 
+}
+
+function handicap_calc() {
+    let p1 = document.getElementById('p1').value
+    let p2 = document.getElementById('p2').value
+
+    // document.getElementById('output').innerHTML =
+}
+
+// rank = "".join(filter(str.isdigit, rank_input))
