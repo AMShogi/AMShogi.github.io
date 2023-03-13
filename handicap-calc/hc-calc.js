dec00a3cbe44185b868133812e9d549231848938
Little-Mage <lm4ssw@gmail.com>
2023-03-12 21:06:01 -0600

    1600

--- handicap-calc/hc-calc.js
+++ handicap-calc/hc-calc.js
@@ -1,12 +1,26 @@
-function handicap_input(message) {
-    let rank_input = 
-}
-
 function handicap_calc() {
     let p1 = document.getElementById('p1').value
-    let p2 = document.getElementById('p2').value
+    // let p2 = document.getElementById('p2').value
 
-    // document.getElementById('output').innerHTML =
+    rank = p1.includes('dan')
+    if (rank = true) {
+        rank = 15
+        return document.getElementById('output').innerHTML = rank
+    }
 }
 
-// rank = "".join(filter(str.isdigit, rank_input))
+// const handicap_dict = {
+//     "0": "No handicap",
+//     "1": "Sente",
+//     "2": "Lance",
+//     "3": "Bishop",
+//     "4": "Rook",
+//     "5": "Rook + Lance",
+//     "6,7": "2-piece",
+//     "8,9": "4-piece",
+//     "10": "6-piece",
+//     "11": "8-Piece",
+//     "12": "10-Piece",
+//     "13": "3 Pawns",
+//     "14,15": "Naked King",
+// }
