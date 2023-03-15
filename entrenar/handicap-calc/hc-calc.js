f78eadb513418e265fd0a4867acbe8b7c69c2b80
Little-Mage <lm4ssw@gmail.com>
2023-03-14 18:26:22 -0600

    hc 2 rank

--- entrenar/handicap-calc/hc-calc.js
+++ entrenar/handicap-calc/hc-calc.js
@@ -8,6 +8,7 @@ function handicap_input(text) {
         return Math.abs(rank - 15)
     } else {
         return alert('Please include kyu or dan')
+        
     }
 }
 
@@ -37,9 +38,14 @@ function handicap_calc() {
     let rank_dif = uwate - shitate
     // let result = document.getElementById('output').innerHTML
     for (const [key, value] of Object.entries(handicap_dict)) {
-        if (rank_dif == key) {
-            document.getElementById('rank dif').innerHTML = 'Rank difference: ' + key
+        if (key.includes(rank_dif)) {
+            document.getElementById('rank dif').innerHTML = 'Rank difference: ' + rank_dif
             document.getElementById('handicap').innerHTML = 'Handicap: ' + value
+        }
+        else if (rank_dif > 15) {
+            
+            document.getElementById('rank dif').innerHTML = 'Rank difference: ' + rank_dif
+            document.getElementById('handicap').innerHTML = 'Handicap: ' + handicap_dict[15]
         }
     }
 }
