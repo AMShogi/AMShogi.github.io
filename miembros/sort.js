0f0bd37dc4f37211919e929bf19856f7c545c317
Little-Mage <lm4ssw@gmail.com>
2023-09-14 00:20:38 -0600

    ez

--- miembros/sort.js
+++ miembros/sort.js
@@ -1,63 +1,27 @@
 // https://www.w3schools.com/howto/howto_js_sort_table.asp
 
-function sort_abc(n) {
-    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
-    table = document.getElementById('shogi')
-    switching = true
-    dir = 'asc'
-    
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById('shogi');
+    switching = true;
+
     while (switching) {
-        switching = false
-        rows = table.rows
+        switching = false;
+        rows = table.rows;
 
         for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false
-            x = rows[i].getElementsByTagName('td')[n]
-            y = rows[i + 1].getElementsByTagName('td')[n]
-
-            if (dir == 'asc') {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true
-                    break
-                }
-            }
-        }
-        if (shouldSwitch) {
-             rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
-             switching = true
-             switchcount ++
-        }
-        // else {
-        //     if (switchcount == 0 && dir == 'asc') {
-        //         dir = 'desc'
-        //         switching = true
-        //     }
-        // }
-    }
-}
-
-function sortRank(n) {
-    let table, rows, switching, i, x, y, shouldSwitch
-    table = document.getElementById('shogi')
-    switching = true
-    
-    while (switching) {
-        switching = false
-        rows = table.rows
-
-        for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false
-            x = rows[i].getElementsByTagName('td')[n]
-            y = rows[i + 1].getElementsByTagName('td')[n]
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName('TD')[0];
+            y = rows[i + 1].getElementsByTagName('TD')[0];
 
             if (Number(x.innerHTML) > Number(y.innerHTML)) {
-                    shouldSwitch = true
-                    break
+                shouldSwitch = true;
+                break;
             }
         }
         if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
-            switching = true
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
         }
     }
 }
