7316dd236b151ea81393731a2f356df79a46d476
Little-Mage <lm4ssw@gmail.com>
2023-09-16 22:47:46 -0600

    sort abc

--- miembros/sort.js
+++ miembros/sort.js
@@ -1,27 +1,63 @@
 // https://www.w3schools.com/howto/howto_js_sort_table.asp
 
-function sortTable() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById('shogi');
-    switching = true;
+function sort_abc(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
+    table = document.getElementById('shogi')
+    switching = true
+    dir = 'asc'
 
     while (switching) {
-        switching = false;
-        rows = table.rows;
+        switching = false
+        rows = table.rows
 
         for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false;
-            x = rows[i].getElementsByTagName('TD')[0];
-            y = rows[i + 1].getElementsByTagName('TD')[0];
+            shouldSwitch = false
+            x = rows[i].getElementsByTagName('td')[n]
+            y = rows[i + 1].getElementsByTagName('td')[n]
 
-            if (Number(x.innerHTML) > Number(y.innerHTML)) {
-                shouldSwitch = true;
-                break;
+            if (dir == 'asc') {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true
+                    break
+                }
             }
         }
         if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
+             rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+             switching = true
+             switchcount ++
         }
+        // else {
+        //     if (switchcount == 0 && dir == 'asc') {
+        //         dir = 'desc'
+        //         switching = true
+        //     }
+        // }
     }
 }
+
+// function sortRank(n) {
+//     var table, rows, switching, i, x, y, shouldSwitch;
+//     table = document.getElementById('shogi');
+//     switching = true;
+
+//     while (switching) {
+//         switching = false;
+//         rows = table.rows;
+
+//         for (i = 1; i < (rows.length - 1); i++) {
+//             shouldSwitch = false;
+//             x = rows[i].getElementsByTagName('TD')[n];
+//             y = rows[i + 1].getElementsByTagName('TD')[n];
+
+//             if (Number(x.innerHTML[0]) > Number(y.innerHTML[0])) {
+//                 shouldSwitch = true;
+//                 break;
+//             }
+//         }
+//         if (shouldSwitch) {
+//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//             switching = true;
+//         }
+//     }
+// }
