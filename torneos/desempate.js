d3a4c50a3b766e514ad0a8bc869bd563439e67ad
Little-Mage <lm4ssw@gmail.com>
2023-09-21 22:15:07 -0600

    penalizaciones

--- torneos/desempate.js
+++ torneos/desempate.js
@@ -5,6 +5,12 @@ let LMsdos = 5
 document.getElementById('dan_sdos').innerHTML = dan_sdos
 document.getElementById('LMsdos').innerHTML = LMsdos
 
+// let LMpromo = 0
+// let Incursor_promo = 0
+
+// document.getElementById('LMpromo').innerHTML = LMpromo
+// document.getElementById('Incursor_promo').innerHTML = Incursor_promo
+
 // Clase B
 let drlennon_promo = 1
 let Gisari_promo = -1
