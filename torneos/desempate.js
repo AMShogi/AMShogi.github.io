2a2ea19c6132d08b4cce81115618a40ce17bfea6
Little-Mage <lm4ssw@gmail.com>
2023-09-22 11:20:41 -0600

    Update desempate.js

--- torneos/desempate.js
+++ torneos/desempate.js
@@ -5,11 +5,11 @@ let LMsdos = 5
 document.getElementById('dan_sdos').innerHTML = dan_sdos
 document.getElementById('LMsdos').innerHTML = LMsdos
 
-// let LMpromo = 0
-// let Incursor_promo = 0
+let LMpromo = -0.5
+let Incursor_promo = -0.5
 
-// document.getElementById('LMpromo').innerHTML = LMpromo
-// document.getElementById('Incursor_promo').innerHTML = Incursor_promo
+document.getElementById('LMpromo').innerHTML = LMpromo
+document.getElementById('Incursor_promo').innerHTML = Incursor_promo
 
 // Clase B
 let drlennon_promo = 1
