569d2c36d6c3a41ffd05e226ae4d09b8e70a149b
Little-Mage <lm4ssw@gmail.com>
2023-09-21 20:28:40 -0600

    update

---
+++ torneos/desempate.js
@@ -0,0 +1,5 @@
+let dan_sdos = 4 + 5
+let LMsdos = 5
+
+document.getElementById('dan_sdos').innerHTML = dan_sdos
+document.getElementById('LMsdos').innerHTML = LMsdos
--- torneos/sdos.js
+++
@@ -1,5 +0,0 @@
-let dan = 4 + 5
-let LM = 5
-
-document.getElementById('dan').innerHTML = dan
-document.getElementById('LM').innerHTML = LM
