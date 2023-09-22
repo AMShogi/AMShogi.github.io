44b8333a24e71a771af4c6ddcc18cadfd1d347bd
Little-Mage <lm4ssw@gmail.com>
2023-09-21 20:44:43 -0600

    C promo

--- torneos/desempate.js
+++ torneos/desempate.js
@@ -1,5 +1,18 @@
+// Clase A
 let dan_sdos = 4 + 5
 let LMsdos = 5
 
 document.getElementById('dan_sdos').innerHTML = dan_sdos
 document.getElementById('LMsdos').innerHTML = LMsdos
+
+// Clase B
+let drlennon_promo = +1
+let Gisari_promo = -1
+
+document.getElementById('drlennon_promo').innerHTML = drlennon_promo
+document.getElementById('Gisari_promo').innerHTML = Gisari_promo
+
+// Clase C
+let angy_promo = 1
+
+document.getElementById('angy_promo').innerHTML = angy_promo
