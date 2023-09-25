b9d0b5f79fed8072891fdf89f0aa2c907dc5cfe1
Little-Mage <lm4ssw@gmail.com>
2023-09-25 00:44:14 -0600

    sdos

--- torneos/meijin.js
+++ torneos/meijin.js
@@ -1,7 +1,7 @@
 // Clase A
 let dan_sdos = 8 + 5 + 5
-let LMsdos = 5 + 5
-let marc_sdos = 8
+let LMsdos = 8 + 6
+let marc_sdos = 8 + 6
 let incursor_sdos = 8
 let gera_sdos = 10 + 8
 
@@ -13,12 +13,14 @@ document.getElementById('gera_sdos').innerHTML = gera_sdos
 
 
 let LMpromo = -0.5
-let incursor_promo = -0.5
+// let incursor_promo = -0.5
 let gera_promo = 1
+let marc_promo = -1
 
 document.getElementById('LMpromo').innerHTML = LMpromo
-document.getElementById('incursor_promo').innerHTML = incursor_promo
+// document.getElementById('incursor_promo').innerHTML = incursor_promo
 document.getElementById('gera_promo').innerHTML = gera_promo
+document.getElementById('marc_promo').innerHTML = marc_promo
 
 // Clase B
 let drlennon_promo = 1
