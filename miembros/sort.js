711adaa4f81cab8cf47de47168e413afab59209c
Little-Mage <lm4ssw@gmail.com>
2023-10-01 14:12:18 -0600

    new

--- miembros/sort.js
+++ miembros/sort.js
@@ -1,40 +1,40 @@
 // https://www.w3schools.com/howto/howto_js_sort_table.asp
 
-function sort_abc(n) {
-    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
-    table = document.getElementById('shogi')
-    switching = true
-    dir = 'asc'
+// function sort_abc(n) {
+//     let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
+//     table = document.getElementById('shogi')
+//     switching = true
+//     dir = 'asc'
 
-    while (switching) {
-        switching = false
-        rows = table.rows
+//     while (switching) {
+//         switching = false
+//         rows = table.rows
 
-        for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false
-            x = rows[i].getElementsByTagName('td')[n]
-            y = rows[i + 1].getElementsByTagName('td')[n]
+//         for (i = 1; i < (rows.length - 1); i++) {
+//             shouldSwitch = false
+//             x = rows[i].getElementsByTagName('td')[n]
+//             y = rows[i + 1].getElementsByTagName('td')[n]
 
-            if (dir == 'asc') {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true
-                    break
-                }
-            }
-        }
-        if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
-            switching = true
-            switchcount ++
-        } 
-        // else {
-        //     if (switchcount == 0 && dir == "asc") {
-        //         dir = "desc";
-        //         switching = true;
-        //     }
-        // }
-    }
-}
+//             if (dir == 'asc') {
+//                 if (x.innerHTML > y.innerHTML) {
+//                     shouldSwitch = true
+//                     break
+//                 }
+//             }
+//         }
+//         if (shouldSwitch) {
+//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+//             switching = true
+//             switchcount ++
+//         } 
+//         // else {
+//         //     if (switchcount == 0 && dir == "asc") {
+//         //         dir = "desc";
+//         //         switching = true;
+//         //     }
+//         // }
+//     }
+// }
 
 // https://www.w3schools.com/js/js_array_sort.asp
 // send dan to top then descending
@@ -66,3 +66,44 @@ function sort_abc(n) {
 //         }
 //     }
 // }
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("shogi");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                x = x.innerText;
+                y = y.innerText;
+                if (x.localeCompare(y, 'en', { numeric: true }) > 0) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                x = x.innerText;
+                y = y.innerText;
+                if (x.localeCompare(y, 'en', { numeric: true }) < 0) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
