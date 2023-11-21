0cafb73d1b7db5a41631bf2f95249609375886ff
Little-Mage <lm4ssw@gmail.com>
2023-11-21 11:27:12 -0700

    Update meijin.js

--- torneos/meijin.js
+++ torneos/meijin.js
@@ -1,19 +1,19 @@
-//let LMpromo =
 let gera_promo = 1
 let marc_promo = -1
+// let LMpromo =
 
-//document.getElementById('LMpromo').innerHTML = LMpromo
-//document.getElementById('incursor_promo').innerHTML = incursor_promo
 document.getElementById('gera_promo').innerHTML = gera_promo
 document.getElementById('marc_promo').innerHTML = marc_promo
+// document.getElementById('LMpromo').innerHTML = LMpromo
 
 // Clase B
-//let incursor_promo =
+// let incursor_promo =
 let drlennon_promo = 1
 let Gisari_promo = -1
 
 document.getElementById('drlennon_promo').innerHTML = drlennon_promo
 document.getElementById('Gisari_promo').innerHTML = Gisari_promo
+// document.getElementById('incursor_promo').innerHTML = incursor_promo
 
 // Clase C
 let angy_promo = 1
