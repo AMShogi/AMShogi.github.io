f516f3716cdb0cf2f19a7671c7792b49fbdd7384
Little-Mage <lm4ssw@gmail.com>
2024-02-18 12:26:49 -0700

    fixed

--- rangos/handicap-calc/hc-calc.js
+++ rangos/handicap-calc/hc-calc.js
@@ -1,8 +1,8 @@
 'use strict'
 
 function handicap_input(text) {
-    const rankInput = text.toLowerCase();
-    const rank = Number.parseInt(rankInput);    
+    let rankInput = text.toLowerCase();
+    let rank = Number.parseInt(rankInput);    
 
     let rank_digit = Boolean
     let rank_letter = Boolean
@@ -26,7 +26,11 @@ function handicap_input(text) {
             throw 'Por favor incluye d, k o b de beginner.'
         }
         if (rankInput.includes('d')) {
-            return (rank > 9) ? rank + 14 : null;
+             if (rank > 9) {
+                throw '>9d no existe.'
+            } else {
+                return rank += 14
+            }
         }
         if (rankInput.includes('b')) {
             return rank = -5
