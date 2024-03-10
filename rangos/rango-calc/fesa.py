bdb0b93e58fc840f51feb47d792880b3cd4d6ae1
Little-Mage <lm4ssw@gmail.com>
2024-03-09 21:50:17 -0700

    rename

---
+++ rangos/rango-calc/fesa.py
@@ -0,0 +1,41 @@
+K_FACTORS = (
+    (720, 40),
+    (1040, 36),
+    (1280, 32),
+    (1560, 28),
+    (1920, 24),
+    (2240, 20),
+    (9999, 16),
+)
+
+def get_elo(tr, games, results):
+    for r, kr in K_FACTORS:
+        if tr < r:
+            k = kr
+            break
+    else:
+        raise ValueError("Invalid rating")
+    change = 0
+    for or_, res in results:
+        ev = 1 / (1 + 10**((or_ - tr) / 400))
+        change += k * max(res - ev, (or_ - tr) / 160 if res == 1 else -ev)
+        if games < 100:
+            change += max(1800 - tr, 0) / 200
+    return round(tr + change)
+
+def main():
+    myelo = int(input("Player Elo: "))
+    mygames = int(input("Player games: "))
+    results = []
+    try:
+        while True:
+            oppelo = int(input("Opponent Elo: "))
+            result = int(input("Result: "))
+            results.append((oppelo, result))
+    except ValueError:
+        pass
+    print("New Elo: {}".format(get_elo(myelo, mygames, results)))
+
+
+if __name__ == '__main__':
+    main()
