4ccfee30650e8e8c5a2e432ad0296552dab4cb03
Little-Mage <lm4ssw@gmail.com>
2024-03-19 11:01:53 -0600

    del alt code

--- miembros/sort.js
+++ miembros/sort.js
@@ -1,72 +1,5 @@
 // https://www.w3schools.com/howto/howto_js_sort_table.asp
 
-// function sort_abc(n) {
-//     let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
-//     table = document.getElementById('shogi')
-//     switching = true
-//     dir = 'asc'
-
-//     while (switching) {
-//         switching = false
-//         rows = table.rows
-
-//         for (i = 1; i < (rows.length - 1); i++) {
-//             shouldSwitch = false
-//             x = rows[i].getElementsByTagName('td')[n]
-//             y = rows[i + 1].getElementsByTagName('td')[n]
-
-//             if (dir == 'asc') {
-//                 if (x.innerHTML > y.innerHTML) {
-//                     shouldSwitch = true
-//                     break
-//                 }
-//             }
-//         }
-//         if (shouldSwitch) {
-//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
-//             switching = true
-//             switchcount ++
-//         } 
-//         // else {
-//         //     if (switchcount == 0 && dir == "asc") {
-//         //         dir = "desc";
-//         //         switching = true;
-//         //     }
-//         // }
-//     }
-// }
-
-// https://www.w3schools.com/js/js_array_sort.asp
-// send dan to top then descending
-
-// function sortRank(n) {
-//     var table, rows, switching, i, x, y, shouldSwitch;
-//     table = document.getElementById('shogi');
-//     switching = true;
-
-//     while (switching) {
-//         switching = false;
-//         rows = table.rows;
-
-//         for (i = 1; i < (rows.length - 1); i++) {
-//             shouldSwitch = false;
-//             x = rows[i].getElementsByTagName('TD')[n];
-//             y = rows[i + 1].getElementsByTagName('TD')[n];
-
-//             Number.parseInt(x.innerHTML, y.innerHTML)
-
-//             if (x.innerHTML > y.innerHTML) {
-//                 shouldSwitch = true;
-//                 break;
-//             }
-//         }
-//         if (shouldSwitch) {
-//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//             switching = true;
-//         }
-//     }
-// }
-
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("shogi");
@@ -108,15 +41,15 @@ function sortTable(n) {
     }
 }
 
-function sortRank(n) {
-    const table = document.getElementById("shogi");
+// function sortRank(n) {
+//     const table = document.getElementById("shogi");
 
-    const sorts = { dan: -1, kyu: 1 };
-    const asorts = Object.keys(sorts);
+//     const sorts = { dan: -1, kyu: 1 };
+//     const asorts = Object.keys(sorts);
 
-    [...table.rows].slice(1).sort((a, b) => (
-        a = a.cells[n].textContent.split('-'), b = b.cells[n].textContent.split('-'),
-        asorts.indexOf(a[n]) - asorts.indexOf(b[n]) || (a[0] - b[0]) * sorts[a[n]]
-    ))
-        .forEach(row => table.appendChild(row));
-}
+//     [...table.rows].slice(1).sort((a, b) => (
+//         a = a.cells[n].textContent.split('-'), b = b.cells[n].textContent.split('-'),
+//         asorts.indexOf(a[n]) - asorts.indexOf(b[n]) || (a[0] - b[0]) * sorts[a[n]]
+//     ))
+//         .forEach(row => table.appendChild(row));
+// }
