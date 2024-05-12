37681cbfbab813ab8f851c36306607f2671fa26d
Little-Mage <lm4ssw@gmail.com>
2024-05-12 09:27:32 -0600

    dif names

--- torneos/meijin.js
+++ torneos/meijin.js
@@ -2,8 +2,8 @@
 let marc_promo = -1
 // let LMpromo =
 
-document.getElementById('gera_promo').innerHTML = gera_promo
-document.getElementById('marc_promo').innerHTML = marc_promo
+// document.getElementById('gera').innerHTML = gera_promo
+document.getElementById('marc').innerHTML = marc_promo
 // document.getElementById('LMpromo').innerHTML = LMpromo
 
 // Clase B
@@ -11,11 +11,11 @@ document.getElementById('marc_promo').innerHTML = marc_promo
 let drlennon_promo = 1
 let Gisari_promo = -1
 
-document.getElementById('drlennon_promo').innerHTML = drlennon_promo
-document.getElementById('Gisari_promo').innerHTML = Gisari_promo
-// document.getElementById('incursor_promo').innerHTML = incursor_promo
+document.getElementById('drlennon').innerHTML = drlennon_promo
+document.getElementById('Gisari').innerHTML = Gisari_promo
+// document.getElementById('incursor').innerHTML = incursor_promo
 
 // Clase C
 let angy_promo = 1
 
-document.getElementById('angy_promo').innerHTML = angy_promo
+document.getElementById('angy').innerHTML = angy_promo
