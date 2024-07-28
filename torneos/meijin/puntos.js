8787d86e2a6dd3a3cc970645ba4b457e347a9fcb
Little-Mage <lm4ssw@gmail.com>
2024-07-28 10:53:03 -0600

    Update puntos.js

--- torneos/meijin/puntos.js
+++ torneos/meijin/puntos.js
@@ -78,29 +78,6 @@ async function fetchData() {
 document.addEventListener('DOMContentLoaded', fetchData);
 
 
-// let gera_promo = 1
-let marc_promo = -1
-// let LMpromo =
-
-// document.getElementById('gera').innerHTML = gera_promo
-document.getElementById('marc').innerHTML = marc_promo
-// document.getElementById('LMpromo').innerHTML = LMpromo
-
-// Clase B
-// let incursor_promo =
-let drlennon_promo = 1
-// let Gisari_promo = -1
-
-document.getElementById('drlennon').innerHTML = drlennon_promo
-// document.getElementById('Gisari').innerHTML = Gisari_promo
-// document.getElementById('incursor').innerHTML = incursor_promo
-
-// Clase C
-// let angy_promo = 1
-
-// document.getElementById('angy').innerHTML = angy_promo
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const tables = document.querySelectorAll('table');
 
@@ -123,3 +100,26 @@ function sortTableByPuntos(table) {
 
     sortedRows.forEach(row => table.appendChild(row));
 }
+
+
+// let gera_promo = 1
+let marc_promo = -1
+// let LMpromo =
+
+// document.getElementById('gera').innerHTML = gera_promo
+document.getElementById('marc').innerHTML = marc_promo
+// document.getElementById('LMpromo').innerHTML = LMpromo
+
+// Clase B
+// let incursor_promo =
+let drlennon_promo = 1
+// let Gisari_promo = -1
+
+document.getElementById('drlennon').innerHTML = drlennon_promo
+// document.getElementById('Gisari').innerHTML = Gisari_promo
+// document.getElementById('incursor').innerHTML = incursor_promo
+
+// Clase C
+// let angy_promo = 1
+
+// document.getElementById('angy').innerHTML = angy_promo
