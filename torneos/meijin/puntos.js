eb1037b6ad66961bd53a9fcebb1e0ee0cdc58a8e
Little-Mage <lm4ssw@gmail.com>
2024-10-10 08:50:40 -0600

    Update puntos.js

--- torneos/meijin/puntos.js
+++ torneos/meijin/puntos.js
@@ -97,30 +97,3 @@ function sortTableByPuntos(table) {
 
     sortedRows.forEach(row => table.appendChild(row));
 }
-
-function calc_avg(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return (sum / arr.length).toFixed(1);
-}
-
-// Clase A
-let gera_avg = (1.2);
-let LMavg = (1.3);
-let marc_avg = (1.4);
-
-document.getElementById('gera_avg').innerHTML = gera_avg;
-document.getElementById('LMavg').innerHTML = LMavg;
-document.getElementById('marc_avg').innerHTML = marc_avg;
-
-// Clase B
-let lennon_promo = 1;
-let ronin_avg = calc_avg([2, 1.7]);
-
-document.getElementById('lennon').innerHTML = lennon_promo;
-document.getElementById('ronin_avg').innerHTML = ronin_avg;
-
-// Clase C
-// let Gisari_promo
