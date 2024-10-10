5b1adf37b4d9da5fb92cefe3f2f1262df177b86a
Little-Mage <lm4ssw@gmail.com>
2024-10-09 23:08:07 -0600

    avg done

--- torneos/ryuo/avg.js
+++ torneos/ryuo/avg.js
@@ -1,4 +1,4 @@
-function calc_avg(arr) {
+function calc(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
@@ -7,10 +7,36 @@ function calc_avg(arr) {
 }
 
 // Div 1
-let gera_avg = calc_avg([10, 4]);
-let LMavg = calc_avg([5, 2]);
-let marc_avg = calc_avg([2, 3]);
+let aniquil = calc([6, 5]);
+let gera = calc([10, 4]);
+let incursor = calc([3, 10]);
+let LM = calc([5, 2]);
+let marc = calc([2, 3]);
 
-document.getElementById('gera_avg').innerHTML = gera_avg;
-document.getElementById('LMavg').innerHTML = LMavg;
-document.getElementById('marc_avg').innerHTML = marc_avg;
+document.getElementById('aniquil').innerHTML = aniquil;
+document.getElementById('gera').innerHTML = gera;
+document.getElementById('incursor').innerHTML = incursor; 
+document.getElementById('LM').innerHTML = LM;
+document.getElementById('marc').innerHTML = marc;
+
+// Div 2
+let grcajsf = calc([9, 6]);
+let lennon = calc([8, 7]);
+let ronin = calc([11, 12]);
+let Tyrf1ng = calc([7, 14]);
+let yan = calc([4, 11]);
+let zuriel = calc([8]);
+
+document.getElementById('grcajsf').innerHTML = grcajsf;
+document.getElementById('lennon').innerHTML = lennon;
+document.getElementById('ronin').innerHTML = ronin;
+document.getElementById('Tyrf1ng').innerHTML = Tyrf1ng;
+document.getElementById('yan').innerHTML = yan;
+document.getElementById('zuriel').innerHTML = zuriel;
+
+// Div 3
+let gisari = calc([12, 15]);
+let workman = calc([9]);
+
+document.getElementById('gisari').innerHTML = gisari;
+document.getElementById('workman').innerHTML = workman;
