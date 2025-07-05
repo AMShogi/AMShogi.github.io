9e8058bc24a2d789cf0756d01ab6143bd06112e9
Little-Mage <lm4ssw@gmail.com>
2025-07-05 17:08:38 -0600

    A > B

--- torneos/meijin/avg.js
+++ torneos/meijin/avg.js
@@ -8,29 +8,27 @@ function calc(arr) {
 
 // Clase A
 let gera_avg = calc([1.2]);
-let hoyamin_avg = calc([2]);
 let LMavg = calc([1.3]);
 let marc_avg = calc([1.4]);
 
 document.getElementById('gera_avg').innerHTML = gera_avg;
-document.getElementById('hoyamin_avg').innerHTML = hoyamin_avg;
 document.getElementById('LMavg').innerHTML = LMavg;
 document.getElementById('marc_avg').innerHTML = marc_avg;
 
 // Clase B
-let incursor_avg = calc([1.5, 2.2]);
+// let incursor_avg = calc([1.5]);
 let lennon_promo = 1;
 let lennon_avg = calc([2.2]);
-let ronin_avg = calc([2, 1.6]);
+let ronin_avg = calc([2]);
 let yan_avg = calc([3]);
-let zuriel_avg = calc([3]);
+// let zuriel_avg = calc([3]);
 
-document.getElementById('incursor_avg').innerHTML = incursor_avg;
+// document.getElementById('incursor_avg').innerHTML = incursor_avg;
 document.getElementById('lennon').innerHTML = lennon_promo;
 document.getElementById('lennon_avg').innerHTML = lennon_avg;
 document.getElementById('ronin_avg').innerHTML = ronin_avg;
 document.getElementById('yan_avg').innerHTML = yan_avg;
-document.getElementById('zuriel_avg').innerHTML = zuriel_avg;
+// document.getElementById('zuriel_avg').innerHTML = zuriel_avg;
 
 // Clase C
 let gisari_avg = calc([2.3]);
@@ -39,3 +37,12 @@ let grcajsf_avg = calc([2.4]);
 
 document.getElementById('gisari_avg').innerHTML = gisari_avg;
 document.getElementById('grcajsf_avg').innerHTML = grcajsf_avg;
+
+// <tr>
+//     <td></td>
+//     <td>Incursor</td>
+//     <td>2-dan</td>
+//     <td id="incursor"></td>
+//     <td></td>
+//     <td id="incursor_avg"></td>
+// </tr>
