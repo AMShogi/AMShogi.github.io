30a2aef98c8a287eb1cd81c7c47a43ac631d8681
Little-Mage <lm4ssw@gmail.com>
2025-07-05 22:37:13 -0600

    incursor

--- torneos/meijin/avg.js
+++ torneos/meijin/avg.js
@@ -16,14 +16,14 @@ document.getElementById('LMavg').innerHTML = LMavg;
 document.getElementById('marc_avg').innerHTML = marc_avg;
 
 // Clase B
-// let incursor_avg = calc([1.5]);
+let incursor_avg = calc([1.5]);
 let lennon_promo = 1;
 let lennon_avg = calc([2.2]);
 let ronin_avg = calc([2]);
 let yan_avg = calc([3]);
 // let zuriel_avg = calc([3]);
 
-// document.getElementById('incursor_avg').innerHTML = incursor_avg;
+document.getElementById('incursor_avg').innerHTML = incursor_avg;
 document.getElementById('lennon').innerHTML = lennon_promo;
 document.getElementById('lennon_avg').innerHTML = lennon_avg;
 document.getElementById('ronin_avg').innerHTML = ronin_avg;
@@ -37,15 +37,6 @@ let grcajsf_avg = calc([2.4]);
 
 document.getElementById('gisari_avg').innerHTML = gisari_avg;
 document.getElementById('grcajsf_avg').innerHTML = grcajsf_avg;
-
-// <tr>
-//  <td></td>
-//  <td>Incursor</td>
-//  <td>2-dan</td>
-//  <td id="incursor"></td>
-//  <td></td>
-//  <td id="incursor_avg"></td>
-// </tr>
 
 // <tr>
 //  <td></td>
