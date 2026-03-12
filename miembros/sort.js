9d8f8cdb3f41067c95ad219b999a87db655dde90
LittleMage <lm4ssw@gmail.com>
2026-03-12 15:07:04 -0600

    gera chess

--- miembros/sort.js
+++ miembros/sort.js
@@ -1,8 +1,4 @@
-const LC_inflation = 200
-
-let gera_chess = 1727 - LC_inflation
-
-document.getElementById('gera_chess').innerHTML = gera_chess
+// const LC_inflation = 200
 
 // https://www.w3schools.com/howto/howto_js_sort_table.asp
 
