5e6f60476c14fa79877f525f7b6464657381e63d
LittleMage <lm4ssw@gmail.com>
2026-05-01 19:36:27 -0600

    18 players

--- torneos/python/random-pair.py
+++ torneos/python/random-pair.py
@@ -1,23 +1,43 @@
 import random
-players = input("Enter players:\n").split()
 
-lower_seed_1 = players[0]
-lower_seed_2 = players[1]
+# Read players from players.txt
+with open("torneos/python/players.txt", "r") as file:
+    players = file.read().split('\n')
 
-remaining_players = sorted(players[2:])
+first_five = sorted(players[:5])
 
-bye_player = random.choice(remaining_players)
+lower_seeds = random.sample(first_five, 4)
 
-remaining_players.remove(bye_player)
+remaining_players = [p for p in players if p not in lower_seeds]
+
+remaining_players.sort()
+
+bye_players = random.sample(remaining_players, 2)
+
+for player in bye_players:
+    remaining_players.remove(player)
 
 random.shuffle(remaining_players)
+
+random.shuffle(lower_seeds)
+lower_seed_matches = [
+    (lower_seeds[0], lower_seeds[1]),
+    (lower_seeds[2], lower_seeds[3])
+]
 
 pairings = []
 for i in range(0, len(remaining_players), 2):
     pairings.append((remaining_players[i], remaining_players[i + 1]))
 
-print('/n' + 'bye player:', bye_player)
+# Output
+print("Lower seeds")
+for p1, p2 in lower_seed_matches:
+    print(f"{p1} vs {p2}")
 
-print("\n" + 'Random Pairings')
+print("\nBye players")
+for player in bye_players:
+    print(player)
+
+print("\nMain pairings")
 for p1, p2 in pairings:
     print(f"{p1} vs {p2}")
